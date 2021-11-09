62f4e83771249a16089c661dd2301f725fcee9a7
Salman Setiawan <11191066@student.itk.ac.id>
2021-11-09 15:38:36 +0800

    nambah picture untuk landing page

--- src/components/CallText.jsx
+++ src/components/CallText.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const CallText = () => {
     return (
         <div>
-            <Link to="/" className="py-4 px-0 bg-transparent rounded-md text-purple-600 text-l font-bold">
+            <Link to="/" className="py-4 px-0 bg-transparent rounded-md text-purple-600 hover:text-pink-600 text-l font-bold">
                 Create account here
             </Link>
         </div>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import CallButton from "../components/CallButton";
 import CallText from "../components/CallText";
-import HeadImg from "../images/experts-header.svg";
+import HeadImg from "../images/home/landing-header.svg";
 import FeatureCard from "../components/FeatureCard";
 
 import React from "react";
@@ -8,7 +8,7 @@ import React from "react";
 export default function LandingPage() {
   return (
     <div className="px-48 py-20">
-      <div className="flex flex-row justify-between items-center gap-x-5">
+      <div className="flex flex-row justify-between items-center gap-x-6">
         <div className="flex flex-col">
           <p className="text-4xl font-bold">
             The right care at
@@ -26,14 +26,32 @@ export default function LandingPage() {
             <CallText />
           </div>
         </div>
-        <img className="h-96" src={HeadImg} alt="" />
+        <img className="h-80" src={HeadImg} alt="" />
+      </div>
+      <div className="pt-60">
+        <div className="flex flex-row justify-between items-center gap-x-6">
+          <div className="flex flex-col">
+            <p className="text-4xl font-bold">
+              Who we are?
+            </p>
+            <p className="pt-6 text-xl mr-8">
+              Our platform empowers peoples with
+              clinically-backed digital tools for proactively
+              improving mental wellbeing.
+            </p>
+          </div>
+          <img className="h-80" src={HeadImg} alt="" />
+        </div>
       </div>
       <div className="py-60">
-        <div className="flex flex-row justify-between items-center gap-x-8">
+        <div className="text-center font-black text-3xl">
+          Feature
+        </div>
+        <div className="pt-12 flex flex-row justify-between items-center gap-x-8">
           {/* Ganti judul video dan foto disini */}
-          <FeatureCard title="Videos" pic="03-video.svg"/>
-          <FeatureCard title="Experts" pic="02-experts.svg"/>
-          <FeatureCard title="Blogs" pic="01-blog.svg"/>
+          <FeatureCard title="Blogs" pic="landing-blogs.svg"/>
+          <FeatureCard title="Videos" pic="landing-videos.svg"/>
+          <FeatureCard title="Experts" pic="landing-experts.svg"/>
         </div>
       </div>
     </div>
