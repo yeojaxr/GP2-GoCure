0ee62374b40a4aa225bd6afefc1f76929898eb06
Salman Setiawan <11191066@student.itk.ac.id>
2021-11-12 21:30:50 +0800

    mengganti logo pada side navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
+import HomeImg from '../images/navbar/home.svg'
 import BlogsImg from '../images/navbar/blogs.svg'
 import VideosImg from '../images/navbar/videos.svg'
 import ExpertsImg from '../images/navbar/experts.svg'
-import ProfileImg from '../images/home/avatar-toji.png'
+import LogoutImg from '../images/navbar/logout.svg'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className="">
             <div className="flex flex-col items-center">
-                <Link className="pt-12 pb-4" to="/">
-                    <img className="h-6" src={BlogsImg} alt="" />
+                <Link className="pt-12 pb-2" to="/">
+                    <img className="h-10" src={HomeImg} alt="" />
                 </Link>
-                <Link className="py-4" to="/">
-                    <img className="h-6" src={VideosImg} alt="" />
+                <Link className="py-2" to="/">
+                    <img className="h-10" src={BlogsImg} alt="" />
                 </Link>
-                <Link className="py-4" to="/">
-                    <img className="h-6" src={ExpertsImg} alt="" />
+                <Link className="py-2" to="/">
+                    <img className="h-10" src={VideosImg} alt="" />
+                </Link>
+                <Link className="py-2" to="/">
+                    <img className="h-10" src={ExpertsImg} alt="" />
                 </Link>
 
                 <Link className="py-12" to="/">
-                    <img className="w-20 md:w-12" src={ProfileImg} alt="" />
+                    <img className="h-10" src={LogoutImg} alt="" />
                 </Link>
             </div>
         </div>
