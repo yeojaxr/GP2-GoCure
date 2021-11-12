4cb95e0ce4eca5c3d5c3442de6b9a3edc5db1509
Salman Setiawan <11191066@student.itk.ac.id>
2021-11-12 23:16:21 +0800

    nambah page not found sama videoplay

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    return (
+        <div className="bg-purple-700 h-screen text-white">
+            <div className="flex h-screen items-center justify-center gap-x-8">
+            <div className="font-bold text-4xl">
+                404
+            </div>
+            <div className="text-lg font-medium">
+                Not Found
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default PageNotFound
--- src/pages/Videoplay.jsx
+++ src/pages/Videoplay.jsx
@@ -1,9 +1,58 @@
+import Card from "../components/Card";
 import React from 'react'
 
 const Videoplay = () => {
     return (
         <div>
-            
+            {/* ADJUST WIDTH */}
+            <div className="px-10 md:px-28 xl:px-30 2xl:px-60">
+                {/* PLACE VIDEO HERE */}
+                <div>
+
+                </div>
+
+                {/* HEAD */}
+                <div className="pt-16 font-bold text-3xl">
+                    Title
+                </div>
+
+                {/* CONTENT */}
+                <div className="py-10 grid grid-cols-1 md:grid-cols-3 gap-x-6">
+
+                    <div className="md:hidden block pb-12">
+                        <p className="text-md font-bold">Salman Setiawan</p>
+                        <p className="text-sm pt-2">Kamis, 24 Oktober 2021</p>
+                    </div>
+
+                    {/* ARTICLE */}
+                    <div className="col-span-2 text-sm md:text-md leading-relaxed text-justify">
+                        Lorem Ipsum
+                    </div>
+                    
+                    {/* OTHER ARTICLE */}
+                    <div className="px-0 md:px-10 text-md leading-relaxed text-justify">
+                        <div className="hidden md:block">
+                            <p className="text-md font-bold">Salman Setiawan</p>
+                            <p className="text-sm pt-2">Kamis, 24 Oktober 2021</p>
+                        </div>
+                        <div className="pt-2 md:pt-10 font-bold">
+                            Other Article
+                        </div>
+                        <div className="pt-3">
+                            <div className="pt-3">
+                                <Card />
+                            </div>
+                            <div className="pt-3">
+                                <Card />
+                            </div>
+                            <div className="pt-3">
+                                <Card />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
