b1379d62162d17955fda360b609efc6b47666d97
Salman Setiawan <11191066@student.itk.ac.id>
2021-11-13 13:04:04 +0800

    install axios, nambahin logo di loading dan notfound page

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import LogoImg from '../images/logo-white.svg'
 import Loading from '../components/Loading'
 
 export const LoadingPage = () => {
     return (
         <div className="bg-purple-700 h-screen">
-            <div className="flex flex-col h-screen items-center justify-center">
-            <Loading />
-            <div className="pt-6 text-white text-lg font-medium">
-                Please Wait
-            </div>
+            <div className="flex flex-col h-screen">
+                <div className="flex flex-col h-screen items-center justify-center">
+                <Loading />
+                <div className="pt-6 text-white text-xl font-bold select-none">
+                    Please Wait
+                </div>
+                </div>
+                <div className="flex justify-center pb-9">
+                    <img className="h-8" src={LogoImg} alt="" />   
+                </div>
             </div>
         </div>
     )
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,15 +1,23 @@
+import LogoImg from '../images/logo-white.svg'
 import React from 'react'
 
 const PageNotFound = () => {
     return (
         <div className="bg-purple-700 h-screen text-white">
-            <div className="flex h-screen items-center justify-center gap-x-8">
-            <div className="font-bold text-4xl">
-                404
-            </div>
-            <div className="text-lg font-medium">
-                Not Found
-            </div>
+            <div className="flex flex-col h-screen">
+                <div className="flex flex-col h-screen items-center justify-center">
+                    <div className="flex gap-x-8 items-center select-none">
+                        <div className="font-bold text-5xl">
+                            404
+                        </div>
+                        <div className="text-xl font-bold">
+                            Not Found
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center pb-9">
+                        <img className="h-8" src={LogoImg} alt="" />   
+                </div>
             </div>
         </div>
     )
